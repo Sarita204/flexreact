134046f11620e26e9f5924af4f9936a07910fd3a
sarita <aritasp204@gmail.com>
2022-07-24 22:26:08 +0530

    task1 completed

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Task1 from './activity/task1'
+
+
+function App() {
+  return (
+    <div className="App">
+    <Task1 />
+    </div>
+  );
+}
+
+export default App;
---
+++ task1.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Task1 extends Component {
+    render(){
+        return(
+            <div className="Main-container">
+                <div className="Section-1">section-1</div>
+                <div className="Section-2">
+                    
+                    <div className="Part-1">Part-1</div>
+                    <div className="Part-2">Part-2</div>
+                </div>
+            </div>
+        );
+    }
+}
+export default Task1;
